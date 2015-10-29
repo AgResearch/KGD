5760bbd489d938b1d26b9ca5893021f36a05a796
Marcus Davy <marcus.davy@plantandfood.co.nz>
2015-10-29 14:47:08 +1300

    example data for GBSPedAssign.R

--- GBSRun.R
+++ GBSRun.R
@@ -7,12 +7,12 @@ source("GBS-Chip-Gmatrix.R")
 Gfull <- calcG()
 GHWdgm.05 <- calcG(which(HWdis > -0.05),"HWdgm.05", npc=4)  # recalculate using Hardy-Weinberg disequilibrium cut-off at -0.05
 
-pedfile <- "Ped-GBS.csv"
-groupsfile <- "Ped-Groups.csv"
+pedfile    <- "Example/Ped-GBS.csv"
+groupsfile <- "Example/Ped-Groups.csv"
 
 rel.thresh <- 0.2
 GCheck <- "GHWdgm.05$G5"
-source("/Code/GBSPedAssign.R")
+source("GBSPedAssign.R")
 
 #G5 <- GHWdgm.05$G5
 #save(G5,file="G5.RData")
