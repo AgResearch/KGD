d8113e2701b75a94207340b8e684009bd913a9cd
Simon Guest <simon.guest@agresearch.co.nz>
2025-01-20 11:14:23 +1300

    Bump flake for NixOS 24.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Naive Nix packaging for KGD";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
     flake-utils.url = "github:numtide/flake-utils";
   };
