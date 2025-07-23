b8823573db041927877bbc1249ead3eceac4f06c
Simon Guest <simon.guest@agresearch.co.nz>
2025-07-24 09:46:12 +1200

    Nix: bump to nixpkgs-unstable and expose required R packages
    The previous constraint from gbs-prism to use an older version of
    nixpkgs has been removed, and the advantage of using nixpkgs-unstable
    is that it has the fixed Rcpp that we need.
    
    Exposing the R packages directly seems now to be required in order to
    use KGD both in gbs-prism and downstream of that in gbs-uber-keyfiles.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Naive Nix packaging for KGD";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     flake-utils.url = "github:numtide/flake-utils";
   };
@@ -19,62 +19,23 @@
             inherit system;
           };
 
-          # we need a recent Rcpp with this fix:
-          # https://github.com/RcppCore/Rcpp/pull/1346
-          recent-Rcpp = pkgs.rPackages.buildRPackage {
-            name = "Rcpp";
-
-            version = "1.0.13.6";
-
-            src = pkgs.fetchFromGitHub {
-              owner = "RcppCore";
-              repo = "Rcpp";
-              rev = "83e640b55aeaeba8a746d0da6152cabe8af41154";
-              hash = "sha256-x0s9BRIAxxZmNFXRYwtpmT5asD4+mhIUTqwYsaPOgi4=";
-            };
-          };
-
-          # TODO: it should be possible to simply override the Rcpp dependency for RcppArmadillo in nixpkgs,
-          # but I didn't work out how to do that
-          RcppArmadillo-with-recent-Rcpp = pkgs.rPackages.buildRPackage {
-            name = "RcppArmadillo";
-
-            version = "0.12.8.1.0";
-
-            src = pkgs.fetchFromGitHub {
-              owner = "RcppCore";
-              repo = "RcppArmadillo";
-              rev = "8abe7be9fc4dd7c1d2b02ed200707232d6fd1f09"; # 0.12.8.1.0
-              hash = "sha256-+Li4ln/4ZyBY+I8S8X4uSmFaG1D3q5UJOJJB5pLRubo=";
-            };
-
-            propagatedBuildInputs = [ recent-Rcpp ];
-          };
-
-
-          KDG-R = pkgs.rWrapper.override
-            {
-              packages = with pkgs.rPackages; [
-                RcppArmadillo-with-recent-Rcpp
-                data_table
-                R_utils
-                plotly
-                heatmaply
-                parallelDist
-                MethComp
-                MASS
-              ];
-            };
+          KGD-rPackages = with pkgs.rPackages;
+            [
+              RcppArmadillo
+              data_table
+              R_utils
+              plotly
+              heatmaply
+              parallelDist
+              MethComp
+              MASS
+            ];
 
           KDG-src = pkgs.stdenv.mkDerivation {
             pname = "KDG-src";
-            version = "1.2.2";
+            version = "1.3.1";
 
             src = ./.;
-
-            buildInputs = [ KDG-R ];
-
-            propagatedBuildInputs = [ KDG-R ];
 
             nativeBuildInputs = [ pkgs.dos2unix ];
 
@@ -95,6 +56,7 @@
         {
           packages = {
             src = KDG-src;
+            rPackages = KGD-rPackages;
           };
         });
 }
